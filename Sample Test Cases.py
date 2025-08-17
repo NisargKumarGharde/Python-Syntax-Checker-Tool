035730f22957937b8bf3989fca3b99310571ce00
Nisarg Kumar Gharde <23f3000632@ds.study.iitm.ac.in>
2025-08-17 15:40:12 +0530

    Sample Test Cases.py

--- Sample Test Cases.py
+++ Sample Test Cases.py
@@ -1,3 +1,43 @@
-# print("Hello to the viewer");
+print("Hello to the viewer");
 
-print("Hello World"
+# NOTE: Please comment/uncomment respectively to try all syntaxes one by one.
+
+#1. Invalid Syntax
+# print("Hello World"
+
+#2. Indentation error
+'''
+def my_function():
+print("This line has incorrect indentation")
+'''
+
+#3. Keyword error
+'''
+for = 10
+print(for)
+'''
+
+#4. Bracket error
+'''
+my_list = [1, 2, 3, 4, 5
+print("The list was created without a closing bracket")
+'''
+
+# Some valid code syntaxes
+
+#1.
+#print("Hello World")
+
+#2. Complex Script
+'''
+import sys
+
+def check_platform():
+    platform = sys.platform
+    if "win" in platform:
+        print("Running on Windows OS.")
+    else:
+        print(f"Running on {platform}.")
+
+check_platform()
+'''
